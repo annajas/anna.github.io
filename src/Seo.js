fa1d39107258e9cdc48a3de22b9f4bb60206117f
Taimor <mtaimorkhan@gmail.com>
2021-09-08 14:52:39 -0400

    updated SEO

--- src/Seo.js
+++ src/Seo.js
@@ -4,12 +4,34 @@ import {Helmet} from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
 const SEO = ({title, description}) => {
+    
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                    description
+                }
+            }
+        }
+    `);
+    
     return (
-        <Helmet htmlAttributes={{lang: `en`}}>
+        <Helmet 
+            htmlAttributes={{lang: `en`}}
+            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+            >
             <title>
                 {title}
             </title>
-            <meta name='description' content={description} />
+            <meta 
+                name='description' 
+                content={description || data.site.siteMetadata.description} />
+            <meta name='og:title' content={title} />
+            <meta
+                name='og:description'
+                content={description || data.site.siteMetadata.description} />
+            <meta name='og:type' content='website' />
         </Helmet>
     );
 }
