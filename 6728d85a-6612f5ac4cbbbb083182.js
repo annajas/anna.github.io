026501ebf960ea3fe6048387aaab12ad72ac11af
Taimor <mtaimorkhan@gmail.com>
2021-09-10 15:28:57 -0400

    Updates

---
+++ 6728d85a-6612f5ac4cbbbb083182.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkanna_website=self.webpackChunkanna_website||[]).push([[158],{9845:function(t,e,n){n.d(e,{tzA:function(){return u}});var a=n(1046);function u(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M23,20 L23,6 L12,15 L1,6 L1,20 L23,20 Z M12,12 L22,4 L2,4 L12,12 Z"}}]})(t)}}}]);
+//# sourceMappingURL=6728d85a-6612f5ac4cbbbb083182.js.map
